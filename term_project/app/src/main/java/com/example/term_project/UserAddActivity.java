b8b8cbaee4c27d0261e69ab67e240167397b82e7
haZuny <gkwns5791@naver.com>
2023-06-04 21:36:45 +0900

    사용자 추가 중복 방지

--- term_project/app/src/main/java/com/example/term_project/UserAddActivity.java
+++ term_project/app/src/main/java/com/example/term_project/UserAddActivity.java
@@ -11,11 +11,15 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class UserAddActivity extends AppCompatActivity {
+    GlobalVar globalVar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_add_page);
+        
+        // 전역변수 가져오기
+        globalVar = (GlobalVar) getApplicationContext(); 
 
         // 컴포넌트
         Button button_addUser = findViewById(R.id.userAdd_button_addUser);
@@ -29,16 +33,20 @@ public class UserAddActivity extends AppCompatActivity {
         button_addUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String querry = String.format("INSERT INTO USER VALUES('%s')", editText_userName.getText());
-                dbWriter.execSQL(querry);
-                try {
+                String text = String.valueOf(editText_userName.getText());
+                // 닉네임 사용중일 경우
+                if (globalVar.getUserList().contains(text)){
+                    Toast.makeText(getApplicationContext(), "이미 사용 중인 이름 입니다.", Toast.LENGTH_SHORT).show();
+                }
+                // 닉네임 추가
+                else{
+                    String querry = String.format("INSERT INTO USER VALUES('%s')", text);
+                    dbWriter.execSQL(querry);
                     Toast.makeText(getApplicationContext(), "사용자가 추가되었습니다.", Toast.LENGTH_SHORT).show();
                     // 유저 선택 페이지로 이동
                     Intent intent = new Intent(getApplicationContext(), UserSelectActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
-                } catch (Exception e){
-                    Toast.makeText(getApplicationContext(), "이미 사용중인 이름입니다.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
