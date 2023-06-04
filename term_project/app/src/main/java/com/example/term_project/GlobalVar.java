f71829365c2fd20f48fede6d845948acb76c0b74
haZuny <gkwns5791@naver.com>
2023-06-04 21:32:25 +0900

    전역변수 구현

---
+++ term_project/app/src/main/java/com/example/term_project/GlobalVar.java
@@ -0,0 +1,44 @@
+package com.example.term_project;
+
+import android.app.Application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GlobalVar extends Application {
+    List<String> userList = new ArrayList<>();  // 사용자 목록
+    int id;     // Plan id
+    String user_id;
+    String title;
+    String day;
+    String s_time;
+    String e_time;
+    int expense;    // 지출액
+
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    // 사용자 리스트 추가
+    int addUserToList(String userName){
+        if (userList.contains(userName)){
+            return -1;
+        }
+        userList.add(userName);
+        return 0;
+    }
+
+    // Getter
+    List<String> getUserList(){
+        return userList;
+    }
+
+    // Setter
+    void setUserList(List<String> userList){
+        this.userList = userList;
+    }
+
+
+}
--- term_project/app/src/main/java/com/example/term_project/UserSelectActivity.java
+++ term_project/app/src/main/java/com/example/term_project/UserSelectActivity.java
@@ -15,13 +15,19 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class UserSelectActivity extends AppCompatActivity {
-    List<String> usersList = new ArrayList<>();
+    // 전역변수
+    GlobalVar globalVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_select_page);
+
+        // 전역변수 가져오기
+        globalVal = (GlobalVar)getApplicationContext();
         
         // 컴포넌트
         Button button_addUser = findViewById(R.id.userSelect_button_addUser);
@@ -35,11 +41,11 @@ public class UserSelectActivity extends AppCompatActivity {
 
         // DB에서 사용자 정보 읽어오기, 리스트뷰에 추가
         while(cursor.moveToNext()){
-            usersList.add(cursor.getString(0));
+            globalVal.addUserToList(cursor.getString(0));
         }
 
         // 리스트뷰 어댑터 추가
-        ArrayAdapter arrayAdapter=new ArrayAdapter(UserSelectActivity.this, android.R.layout.simple_expandable_list_item_1,usersList);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(UserSelectActivity.this, android.R.layout.simple_expandable_list_item_1, globalVal.getUserList());
         userList.setAdapter(arrayAdapter);
 
         // 사용자 추가 버튼 동작 구현
@@ -56,7 +62,7 @@ public class UserSelectActivity extends AppCompatActivity {
         button_manageUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "text: "+usersList.get(0), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "text: "+ globalVal.getUserList().get(0), Toast.LENGTH_SHORT).show();
             }
         });
     }
